266979a5e55c3859599f19405ac79134a7c503cc
Daniel Wong <danielwong2268@gmail.com>
2025-05-20 23:48:59 -0700

    handle workflow updated v2 event

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,26 @@
 import { type EmitterSubscription } from 'react-native'
 import KanmonModule, { WebViewEventEmitter } from './KanmonModule'
-import {
-  type AllSteps,
-  type Section,
-  type WorkflowStep,
-} from './types/General.types'
-import {
-  OnEventCallbackEventType,
-  type UserStateWithActionMessage,
-} from './types/OnEventCallbackEvent.types'
+import { OnEventCallbackEventType } from './types/OnEventCallbackEvent.types'
 import {
   ReceivedFromKanmonActions,
   type ReceivedFromKanmonMessage,
 } from './types/ReceivedFromBizexMessages.types'
+import {
+  SentToKanmonConnectActions,
+  type SentToKanmonConnectMessage,
+} from './types/SentToKanmonConnectMessage.types'
 import {
   ExternalProductType,
   KanmonConnectComponent,
   KanmonConnectEnviroment,
   type KanmonConnectParams,
 } from './types/types'
-import {
-  type SentToKanmonConnectMessage,
-  SentToKanmonConnectActions,
-} from './types/SentToKanmonConnectMessage.types'
 
-export * from './types/types'
+export * from './types/General.types'
 export * from './types/OnEventCallbackEvent.types'
 export * from './types/ReceivedFromBizexMessages.types'
 export * from './types/SentToKanmonConnectMessage.types'
-export * from './types/General.types'
+export * from './types/types'
 
 const validateParams = ({ connectToken }: KanmonConnectParams) => {
   if (!connectToken || typeof connectToken !== 'string') {
@@ -60,139 +52,6 @@ const validateShowArgs = (showArgs: ShowArgs) => {
     !sessionToken
   ) {
     throw new Error(`sessionToken must be a string.`)
-  }
-}
-
-const stepToUserState = (
-  step: AllSteps,
-  section: Section,
-): UserStateWithActionMessage => {
-  switch (step) {
-    case 'BLOCKED_ON_PRIMARY_OWNER':
-      return {
-        actionMessage: 'Check back soon',
-        userState: 'OTHER_USER_INPUT_REQUIRED',
-        actionRequired: false,
-        section,
-      }
-    case 'PRIMARY_OWNER_CONFLICT':
-      return {
-        actionMessage: 'In manual review',
-        userState: 'IN_MANUAL_REVIEW',
-        actionRequired: false,
-        section,
-      }
-    case 'ONBOARDING.START_FLOW':
-      return {
-        actionMessage: 'Need financing?',
-        userState: 'START_FLOW',
-        actionRequired: true,
-        section,
-      }
-    case 'SELECT_USER_ROLES':
-    case 'ONBOARDING.SELECT_PRODUCTS':
-    case 'ONBOARDING.COLLECT_PERSONAL_DETAILS':
-    case 'ONBOARDING.COLLECT_SECONDARY_BUSINESS_OWNER_DETAILS':
-    case 'ONBOARDING.COLLECT_BUSINESS_DETAILS':
-    case 'COLLECT_BANK_STATEMENTS':
-    case 'COLLECT_BANK_INFO':
-    case 'ONBOARDING.COLLECT_PERSONAL_PLAID':
-    case 'ONBOARDING.COLLECT_PLAID':
-    case 'ONBOARDING.COLLECT_RAILZ':
-    case 'ONBOARDING.COLLECT_EXISTING_DEBT':
-    case 'ONBOARDING.COLLECT_REFINANCE_OPTIONS':
-    case 'ONBOARDING.COLLECT_CONSENT':
-      return {
-        actionMessage: `You're almost there!`,
-        userState: 'USER_INPUT_REQUIRED',
-        actionRequired: true,
-        section,
-      }
-    case 'OFFERS_PENDING':
-    case 'WAITING_FOR_OFFERS':
-      return {
-        actionMessage: 'Offer processing',
-        userState: 'WAITING_FOR_OFFERS',
-        actionRequired: false,
-        section,
-      }
-    case 'LOAN_APPLICATION_INCOMPLETE':
-      return {
-        actionMessage: 'Loan application incomplete',
-        userState: 'LOAN_APPLICATION_INCOMPLETE',
-        actionRequired: false,
-        section,
-      }
-    case 'LOAN_APPLICATION_WITHDRAWN':
-      return {
-        actionMessage: 'Loan application withdrawn',
-        userState: 'LOAN_APPLICATION_WITHDRAWN',
-        actionRequired: false,
-        section,
-      }
-    case 'NO_OFFERS_EXTENDED':
-      return {
-        actionMessage: 'No offers',
-        userState: 'NO_OFFERS_EXTENDED',
-        actionRequired: false,
-        section,
-      }
-    case 'ONBOARDING_ERROR':
-      return {
-        actionMessage: 'Error',
-        userState: 'IN_MANUAL_REVIEW',
-        actionRequired: false,
-        section,
-      }
-    case 'REQUEST_ADDITIONAL_USER_INPUT':
-      return {
-        actionMessage: 'Action required',
-        userState: 'USER_INPUT_REQUIRED',
-        actionRequired: true,
-        section,
-      }
-    case 'DISPLAY_OFFERS':
-      return {
-        actionMessage: 'View your offers 🎉',
-        userState: 'VIEW_OFFERS',
-        actionRequired: true,
-        section,
-      }
-    case 'COLLECT_LEGAL_AGREEMENTS':
-    case 'COLLECT_TAX_ID':
-    case 'COLLECT_PERSONA':
-    case 'COLLECT_PERSONAL_PHONE_NUMBER':
-    case 'SELECT_PRIMARY_BANK_ACCOUNT':
-      return {
-        actionMessage: `You're almost there!`,
-        userState: 'USER_INPUT_REQUIRED',
-        actionRequired: true,
-        section,
-      }
-    case 'OFFER_ACCEPTED':
-    case 'ISSUED_PRODUCT_CREATED':
-      return {
-        actionMessage: 'Pending disbursement',
-        userState: 'OFFER_ACCEPTED',
-        actionRequired: false,
-        section,
-      }
-    case 'OFFER_REFRESH_PENDING':
-    case 'OFFERS_EXPIRED':
-      return {
-        actionMessage: 'Offer expired',
-        userState: 'OFFERS_EXPIRED',
-        actionRequired: false,
-        section,
-      }
-    case 'INIT_SERVICING_FLOW':
-    case 'READY_FOR_SERVICING':
-      return {
-        actionMessage: 'My Dashboard',
-        userState: 'SERVICING',
-        actionRequired: true,
-        section,
-      }
   }
 }
 
@@ -263,6 +122,8 @@ const nativeSdk = {
       productSubsetDuringOnboarding: params.productSubsetDuringOnboarding,
     })
 
+    console.log('startKanmon', url)
+
     KanmonModule.start(url)
 
     subscription?.remove()
@@ -273,19 +134,18 @@ const nativeSdk = {
       (message) => {
         const data: ReceivedFromKanmonMessage = JSON.parse(message)
 
+        console.log('DATA', data)
+
         switch (data.action) {
           case ReceivedFromKanmonActions.HIDE:
             params.onEvent?.({
               eventType: OnEventCallbackEventType.HIDE,
             })
             break
-          case ReceivedFromKanmonActions.WORKFLOW_UPDATED:
+          case ReceivedFromKanmonActions.WORKFLOW_UPDATED_V2:
             params.onEvent?.({
               eventType: OnEventCallbackEventType.USER_STATE_CHANGED,
-              data: stepToUserState(
-                data.nextStep as WorkflowStep,
-                data.section,
-              ),
+              data: data.data,
             })
             break
           case ReceivedFromKanmonActions.ERROR:
